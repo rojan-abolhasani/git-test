d87db79aa85c26443abdedb6b679c7be787b56fd
rojan-abolhasani <58859111+rojan-abolhasani@users.noreply.github.com>
2019-12-29 17:40:46 +0330

    create name.c

---
+++ names.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define N 20
+main()
+{
+	char str1[N];
+	int n, i, max=0, j, letter=0, k, flag=1;
+	scanf("%d", &n);
+	for (i=0;i<n;i++)
+	{
+		letter=1;
+		scanf("%s", str1);
+		for (j=0;str1[j]!='\0';j++)
+		{
+			flag=1;
+			for (k=0;k<j;k++)
+			{
+				if (str1[k]==str1[j])
+				{
+					flag=1;
+					break;
+				}
+				else
+				{
+					flag=0;
+				}
+			}
+			if (flag==0)
+			letter ++;
+		}
+		if (letter>=max)
+			max=letter;	
+	}
+		printf("%d", max);
+}
